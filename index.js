23cccbea157783f049a21d1bbf152727520a5802
nadvspb <102213204+nadvspb@users.noreply.github.com>
2022-03-23 10:56:10 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+echo "# nadvspb" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nadvspb/nadvspb.git
+git push -u origin main
